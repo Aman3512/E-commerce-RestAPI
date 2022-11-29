1662b0848535beb2caa6765f0b74153e94f827d5
Aman Rawat <Amansinghrawat361@gmail.com>
2022-11-29 16:44:21 +0530

    ropo created

---
+++ E-commerce-RestAPI/src/main/java/com/ecommerce/ECommerceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceRestApiApplication.class, args);
+	}
+
+}
